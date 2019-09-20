6b10756c07c9d5582c557aa21cca51f9682cf945
Kevin Vuong <c_k.k@hotmail.com>
2019-09-20 21:23:12 +1000

    odin flickr

---
+++ README.md
@@ -0,0 +1,13 @@
+# Odin Flickr
+  
+This project was completed as part of [The Odin Project](https://www.theodinproject.com/courses/ruby-on-rails/lessons/apis#project-2-using-a-third-party-api) curriculum.
+
+The app is a simple photo widget that allows you to display photos from your Flickr feed by searching via user id.
+
+* Ruby version
+
+`2.5.3`
+
+* Rails version
+
+`5.2.3`
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+    @flickr = FlickRaw::Flickr.new
+    if params[:search]
+      unless params[:search][:user_id].blank?
+      #  @photos = nil
+      #else
+        @photos = flickr.photos.search(user_id: params[:search][:user_id])
+      end
+    end
+  end
+
+  private
+    def search_params
+      params.require(:search).permit(:user_id)
+    end
+end
